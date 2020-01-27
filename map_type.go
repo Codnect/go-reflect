12f3442e6c7cb1ceef70a14cb4dc9dea44b467e7
Burak Koken <burakkokenn@gmail.com>
2020-01-27 22:03:41 +0200

    Added a new function to create a new instance of type of map.

--- map_type.go
+++ map_type.go
@@ -1,6 +1,9 @@
 package reflect
 
-import "reflect"
+import (
+	"github.com/codnect/go-one"
+	"reflect"
+)
 
 type MapType struct {
 	typ reflect.Type
@@ -18,4 +21,8 @@ func (t MapType) GetKeyType() Type {
 
 func (t MapType) GetValueType() Type {
 	return newTypeWithType(t.typ.Elem())
+}
+
+func (t MapType) NewInstance() one.One {
+	return reflect.MakeMap(reflect.MapOf(t.typ.Key(), t.typ.Elem()))
 }
